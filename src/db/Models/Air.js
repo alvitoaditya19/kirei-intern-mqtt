6b419b6006439b2276752da5b9ed8cd3d49f6aa1
MuhammadAlvitoAditya <itoa47@gmail.com>
2022-06-10 13:36:24 +0700

    data

--- src/db/Models/Air.js
+++ src/db/Models/Air.js
@@ -3,15 +3,15 @@ const mongoose = require("mongoose");
 const kekeruhanAirSchema = mongoose.Schema({
   kekeruhanAir: {
     type: String,
-    require: [true, "KekeruhanAir Humidity Harus Diisi"],
+    // require: [true, "KekeruhanAir Humidity Harus Diisi"],
   },
   phAir: {
     type: String,
-    require: [true, "PH Air Harus Diisi"],
+    // require: [true, "PH Air Harus Diisi"],
   },
   oksigen: {
     type: String,
-    require: [true, "Oksigen Air Harus Diisi"],
+    // require: [true, "Oksigen Air Harus Diisi"],
   },
 },{ timestamps: true });
 
--- src/db/Models/Tanah.js
+++ src/db/Models/Tanah.js
@@ -3,19 +3,19 @@ const mongoose = require("mongoose");
 let tanahSchema = mongoose.Schema({
   kelembapanTanah: {
     type: String,
-    require: [true, "Kelembapan Harus Diisi"],
+    // require: [true, "Kelembapan Harus Diisi"],
   },
   nitratTanah: {
     type: String,
-    require: [true, "Kelembapan Harus Diisi"],
+    // require: [true, "Kelembapan Harus Diisi"],
   },
   kadarTanah: {
     type: String,
-    require: [true, "Kelembapan Harus Diisi"],
+    // require: [true, "Kelembapan Harus Diisi"],
   },
   phTanah: {
     type: String,
-    require: [true, "Kelembapan Harus Diisi"],
+    // require: [true, "Kelembapan Harus Diisi"],
   },
 },{ timestamps: true });
 
